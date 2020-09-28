087d6ef93dbfc95c7b91371f56adee798bcb5d1c
Karolin Frennert <karolinfrennert@gmail.com>
2020-09-28 21:12:54 +0200

    Emojis

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// selectors
+//Emojis
+const tickEmoji = "✔️";
+const crossEmoji = "❌";
+const trashEmoji = "🗑️";
 
+
+// selectors
 const groceryInput = document.querySelector('.add-food-input');
 const groceryButton = document.querySelector('.add-food-button');
 const groceryList = document.querySelector('.grocery-list');
@@ -18,18 +23,18 @@ function addGrocery(event) {
 errorMessage.innerHTML = "";
 
  //New Div with grocery items
-  const groceryDiv = document.createElement('div');
-  groceryDiv.classList.add("grocery-div");
+const groceryDiv = document.createElement('div');
+groceryDiv.classList.add("grocery-div");
 
 //New Li of the item + min input required
 
   const newGroceyItem = document.createElement('li');
   const inputValue = groceryInput.value;
   if (!inputValue) {
-    errorMessage.innerHTML = "Please enter something u domb";
+    errorMessage.innerHTML = "Please enter something";
     return;
   } 
-  
+
   newGroceyItem.innerText = inputValue;
   newGroceyItem.classList.add("grocery-item")
   
@@ -38,15 +43,15 @@ errorMessage.innerHTML = "";
 
   //Completed button
   const completedButton = document.createElement('button');
-  completedButton.innerText = "Completed";
-  completedButton.classList.add("complete-button");
+  completedButton.innerText = tickEmoji;
+  completedButton.addEventListener('click',taskStatus);
 
   //add button to div
   groceryDiv.appendChild(completedButton);
 
   //Trash button
   const trashButton = document.createElement('button');
-  trashButton.innerText = "Trash";
+  trashButton.innerText = trashEmoji;
   trashButton.classList.add("trash-button");
   trashButton.addEventListener('click', trash);
 
@@ -56,6 +61,16 @@ errorMessage.innerHTML = "";
 }
 
 function trash() {
-    const wrapper = this.closest(".grocery-div");
-    wrapper.remove();
+  this.removeEventListener('click', trash);
+  const wrapper = this.closest(".grocery-div");
+  wrapper.remove();
 };
+
+function taskStatus() {
+      
+  const buttonText = this.innerText;
+  buttonText === crossEmoji 
+    ? this.innerText = tickEmoji
+    : this.innerText = crossEmoji;
+
+}
